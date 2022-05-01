0e48b6e895cdddb2cb100f1dcd5690864aca1c8c
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-01 13:17:19 +0600

    Post API created

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-//App amd Port
+//App and Port
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// DB Info
+const uri =
+  "mongodb+srv://Rakib:<password>@cluster0.c95rx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// Main API function
+
+const run = async () => {
+  try {
+    //Create connection and Connect client
+    await client.connect();
+    const productCollection = client.db("FragranceWMS").collection("Products");
+
+    // Insert data
+    app.post(
+      ("/products",
+      async (req, res) => {
+        const productsData = req.body;
+        const result = await productCollection.insertOne(productsData);
+        res.send(result);
+      })
+    );
+  } finally {
+    // Connection
+  }
+};
+run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Server is runing...");
 });
